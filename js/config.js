dbd2bd6f01efa15b4d19ed3831ce5797617e588e
besooadel <besooxx@gmail.com>
2026-05-13 17:22:04 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,8 +5,7 @@
 // 2. انسخ Project URL و anon/public key
 
 // ⚙️ إعدادات Supabase - مطعم النخبة
-window.SUPABASE_URL = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2Z3N1cnpybnZ6aG9na3ZoY2VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NzM2ODAsImV4cCI6MjA5NDI0OTY4MH0.47fCgS4jiQqYZwaS-ZnlqQhW815kuRDyEp-zD4-B4mo';
-window.SUPABASE_KEY = 'sb_publishable_UVXyikOw03Z2-lwXwG90RA_8PZjHypC';
+sb_publishable_UVXyikOw03Z2-lwXwG90RA_8PZjHypC
 
 // مثال:
 // const SB_URL = 'https://abcdefgh.supabase.co';
