84d006b338a954eb9d448329285c1a4e35306cd6
besooadel <besooxx@gmail.com>
2026-05-13 17:26:47 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,9 +6,9 @@
 
 // ⚙️ إعدادات Supabase - مطعم النخبة
 // ⚙️ إعدادات Supabase - مطعم النخبة
+// ⚙️ إعدادات Supabase - مطعم النخبة
 window.SUPABASE_URL = 'sb_publishable_UVXyikOw03Z2-lwXwG90RA_8PZjHypC';
-window.SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2Z3N1cnpybnZ6aG9na3ZoY2VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NzM2ODAsImV4cCI6MjA5NDI0OTY4MH0.47fCgS4jiQqYZwaS-ZnlqQhW815kuRDyEp-zD4-B4mo';
-
+window.SUPABASE_KEY = 'sb_secret_Wn3BF579JElcJjC4ox30Fw_4FwnjL2l';
 // مثال:
 // const SB_URL = 'https://abcdefgh.supabase.co';
 // const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
